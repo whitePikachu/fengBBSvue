2f1e17e071b7550809be6842254f14187e6f87f7
枫叶秋林 <liuqianpan2018@outlook.com>
2022-11-14 19:03:36 +0800

    支持夜间模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ["vue"],
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: "Icon",
+        }),
+      ],
+    }),
+    Components({
+      resolvers: [
+        IconsResolver({
+          enabledCollections: ["ep"],
+        }),
+        ElementPlusResolver(),
+      ],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
+});
