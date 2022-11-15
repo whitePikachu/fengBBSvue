021a8e86b0675f8212f2199d7e0c71c5c20a23d0
枫叶秋林 <liuqianpan2018@outlook.com>
2022-11-15 10:34:39 +0800

    优化机构

---
+++ src/views/homeHeader/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { Check, Close } from '@element-plus/icons-vue'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <el-switch v-model="isDark"
+             class="mt-2"
+             style="margin-left: 24px"
+             inline-prompt
+             :active-icon="Check"
+             :inactive-icon="Close" />
+</template>
+
+<style scoped>
+</style>
